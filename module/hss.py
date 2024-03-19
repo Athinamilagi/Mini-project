f0f2290609413c6dde21fa06024a8ed0c0b157bd
Athinamilagi <kvigneshvicky6794@gmail.com>
2024-03-19 20:45:27 +0530

    added base64 decode method

--- module/hss.py
+++ module/hss.py
@@ -4,25 +4,27 @@ import cv2
 def load_image(file_path):
     return cv2.imread(file_path)
 
+
 # Function to compare images
 def compare_images(image1, image2):
     # Resize the captured frame to match the size of the reference image
     image2_resized = cv2.resize(image2, (image1.shape[1], image1.shape[0]))
-    
+
     # Convert images to grayscale
     gray_image1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
     gray_image2 = cv2.cvtColor(image2_resized, cv2.COLOR_BGR2GRAY)
-    
+
     # Compute the absolute difference between the two images
     diff = cv2.absdiff(gray_image1, gray_image2)
-    
+
     # Compute the mean squared error (MSE)
-    mse = (diff ** 2).mean()
-    
+    mse = (diff**2).mean()
+
     return mse
 
+
 # Path to the folder containing the reference image
-reference_image_path = './captured_frame.jpg'
+reference_image_path = "./images/received_image.jpg"
 
 # Load the reference image
 reference_image = load_image(reference_image_path)
@@ -40,10 +42,10 @@ mse = compare_images(reference_image, frame)
 print("MSE:", mse)
 
 # Display the captured frame
-cv2.imshow('Authentication', frame)
+cv2.imshow("Authentication", frame)
 
 # If the MSE is below a certain threshold, consider it a match
-if mse < 100:  # Adjusted threshold value
+if mse < 115:  # Adjusted threshold value
     print("User authenticated")
     authenticated = True
 else:
